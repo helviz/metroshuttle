7a32706c0fe2585619c3a1f6e2678804d60f225d
Itianit Jude <doperudxhark@gmail.com>
2024-07-02 18:25:36 +0300

    Modified maps page

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,6 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Maps();
+    return  const Maps();
   }
 }
--- lib/views/maps.dart
+++ lib/views/maps.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 
+class Maps extends StatefulWidget {
+   const Maps({super.key});
 
-class Maps extends StatelessWidget {
-   Maps({super.key});
- 
+  @override
+  State<Maps> createState() => _MapsState();
+}
+
+class _MapsState extends State<Maps> {
+
+  String ? _mapStyle;
+  GoogleMapController ? _myMapController;
+
+  @override
+  void initState() {
+    super.initState();
+   _loadMapStyle();
+  }
+
+   Future<void> _loadMapStyle() async {
+    final style = await rootBundle.loadString('assets/map_style.txt');
+    setState(() {
+      _mapStyle = style;
+    });
+  }
+
    final CameraPosition _sourceDestination = const CameraPosition(
     target: LatLng(0.347596, 32.582520),
     zoom: 14.4746,
   );
-    GoogleMapController ? myMapController;
 
   @override
   Widget build(BuildContext context) {
@@ -24,13 +45,22 @@ class Maps extends StatelessWidget {
             right: 0,
             bottom: 0,
             child: GoogleMap(
-            mapType: MapType.terrain,
+              zoomControlsEnabled: false,
             onMapCreated: (GoogleMapController controller) {
-              myMapController = controller;
-            }, initialCameraPosition: _sourceDestination ),
+              _myMapController = controller;
+              
+              if (_mapStyle != null) {
+                
+                }
+                         }, initialCameraPosition:  _sourceDestination , style: _mapStyle,),
+                         
           ),
 
           buildProfileTile(), 
+          buildTextField(),
+          buildCurrentLocationIcon(),
+          buildNotificationIcon(),
+          buildBottomSheet(),
       ]));
   }
 }
@@ -44,19 +74,19 @@ Widget buildProfileTile() {
       child: Container( 
         child:  Row(
          children: [
-          CircleAvatar(backgroundColor: Colors.grey[300] ,
+          CircleAvatar(backgroundColor: Colors.orange ,
           radius: 30,),
-          SizedBox(width: 15,),
+          const SizedBox(width: 15,),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start, 
             children: [
-              RichText(text: TextSpan(
+              RichText(text: const TextSpan(
                 children: [
                   TextSpan(text: 'Hello, ', style: TextStyle(color: Colors.black,fontSize: 14)),
                   TextSpan(text: 'Martin', style: TextStyle(color: Colors.orange,fontSize: 16))
                   ])),
-                  SizedBox(height: 10,),
-                  Text('Where are you going?',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+                  const SizedBox(height: 10,),
+                  const Text('Where are you going?',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
 
                   ]
             
@@ -65,3 +95,91 @@ Widget buildProfileTile() {
       )
     ));
     }
+
+Widget buildTextField() {
+    return Positioned(
+      top: 170,
+      left: 20,
+      right: 20,
+      child: Container(
+        height: 50,
+        padding: const EdgeInsets.only(left: 15),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  spreadRadius: 4,
+                  blurRadius: 10)
+            ],
+            borderRadius: BorderRadius.circular(8)),
+        child: TextFormField(
+           style: GoogleFonts.poppins(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          decoration: InputDecoration(
+            hintText: 'Search for a destination',
+            hintStyle: GoogleFonts.poppins(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+            suffixIcon: const Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Icon(
+                Icons.search,
+              ),
+            ),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    );
+  }
+
+Widget buildCurrentLocationIcon() {
+  return Align(
+    alignment: Alignment.bottomRight,
+    child: Padding(
+      padding: const EdgeInsets.only(bottom: 30, right: 8),
+      child: CircleAvatar(backgroundColor: Colors.orange,radius: 25,
+      child: IconButton(icon: const Icon(Icons.my_location,color: Colors.white,), onPressed: (){}),
+    ),
+  ));
+}
+
+Widget buildNotificationIcon() {
+  return Align(
+    alignment: Alignment.bottomLeft,
+    child: Padding(padding: const EdgeInsets.only(bottom: 30,left: 8), child: 
+    CircleAvatar(backgroundColor: Colors.orange, radius: 20,
+    child: IconButton(onPressed: () {}, icon: const Icon(Icons.notification_add, color: Colors.white,)),),),
+    );
+}
+
+Widget buildBottomSheet() {
+  return Align(
+    alignment: Alignment.bottomCenter,
+    child: Container(
+      height: 20,width: 250,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            spreadRadius: 4,
+            blurRadius: 10,
+          )
+        ],
+        borderRadius: const BorderRadius.only(topLeft: Radius.circular(12), topRight: Radius.circular(12))
+      ),
+      child: Center(
+        child: Container(
+          height: 4,
+          width: 200,
+          color: Colors.black,
+        ),
+      ),
+    ),
+  );
+}
