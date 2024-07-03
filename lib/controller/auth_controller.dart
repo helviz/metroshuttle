dbd9b0e3ea084f631bc02500005e681916b3c088
helviz <twomoelvisrodney@gmail.com>
2024-07-03 21:08:41 +0300

    controller for authentication 01

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -10,31 +10,7 @@ class AuthController extends GetxController {
   int? resendTokenId;
   bool phoneAuthCheck = false;
   dynamic credentials;
-  //
-  // var isProfileUploading = false.obs;
-  //
-  // bool isLoginAsDriver = false;
-  //
-  // storeUserCard(String number, String expiry, String cvv, String name) async {
-  //   await FirebaseFirestore.instance
-  //       .collection('users')
-  //       .doc(FirebaseAuth.instance.currentUser!.uid)
-  //       .collection('cards')
-  //       .add({'name': name, 'number': number, 'cvv': cvv, 'expiry': expiry});
-  //
-  //   return true;
-  // }
-  //
-  // RxList userCards = [].obs;
-  //
-  // getUserCards() {
-  //   FirebaseFirestore.instance
-  //       .collection('users')
-  //       .doc(FirebaseAuth.instance.currentUser!.uid).collection('cards')
-  //       .snapshots().listen((event) {
-  //     userCards.value = event.docs;
-  //   });
-  // }
+
 
   phoneAuth(String phone) async {
     try {
@@ -62,16 +38,15 @@ class AuthController extends GetxController {
         codeAutoRetrievalTimeout: (String verificationId) {},
       );
     } catch (e) {
-      log("Error occured $e");
+      log("Error occurred $e");
     }
   }
 
   verifyOtp(String otpNumber) async {
     log("Called");
-    PhoneAuthCredential credential =
     PhoneAuthProvider.credential(verificationId: verId, smsCode: otpNumber);
 
-    log("LogedIn");
+    log("LoggedIn");
 
     // await FirebaseAuth.instance.signInWithCredential(credential).then((value) {
     //   decideRoute();
@@ -80,186 +55,6 @@ class AuthController extends GetxController {
     // });
   }
 
-  var isDecided = false;
 
-  // decideRoute() {
-  //   if (isDecided) {
-  //     return;
-  //   }
-  //   isDecided = true;
-  //   print("called");
-  //
-  //   ///step 1- Check user login?
-  //   User? user = FirebaseAuth.instance.currentUser;
-  //
-  //   if (user != null) {
-  //     /// step 2- Check whether user profile exists?
-  //
-  //     ///isLoginAsDriver == true means navigate it to the driver module
-  //
-  //     FirebaseFirestore.instance
-  //         .collection('users')
-  //         .doc(user.uid)
-  //         .get()
-  //         .then((value) {
-  //
-  //
-  //       ///isLoginAsDriver == true means navigate it to driver module
-  //
-  //       if(isLoginAsDriver){
-  //
-  //         if (value.exists) {
-  //           print("Driver HOme Screen");
-  //         } else {
-  //           Get.offAll(() => DriverProfileSetup());
-  //         }
-  //
-  //
-  //       }else{
-  //         if (value.exists) {
-  //           Get.offAll(() => HomeScreen());
-  //         } else {
-  //           Get.offAll(() => ProfileSettingScreen());
-  //         }
-  //       }
-  //
-  //
-  //
-  //     }).catchError((e) {
-  //       print("Error while decideRoute is $e");
-  //     });
-  //   }
-  // }
-
-  // uploadImage(File image) async {
-  //   String imageUrl = '';
-  //   String fileName = Path.basename(image.path);
-  //   var reference = FirebaseStorage.instance
-  //       .ref()
-  //       .child('users/$fileName'); // Modify this path/string as your need
-  //   UploadTask uploadTask = reference.putFile(image);
-  //   TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
-  //   await taskSnapshot.ref.getDownloadURL().then(
-  //         (value) {
-  //       imageUrl = value;
-  //       print("Download URL: $value");
-  //     },
-  //   );
-  //
-  //   return imageUrl;
-  // }
-
-  // storeUserInfo(
-  //     File? selectedImage,
-  //     String name,
-  //     String home,
-  //     String business,
-  //     String shop, {
-  //       String url = '',
-  //       LatLng? homeLatLng,
-  //       LatLng? businessLatLng,
-  //       LatLng? shoppingLatLng,
-  //     }) async {
-  //   String url_new = url;
-  //   if (selectedImage != null) {
-  //     url_new = await uploadImage(selectedImage);
-  //   }
-  //   String uid = FirebaseAuth.instance.currentUser!.uid;
-  //   FirebaseFirestore.instance.collection('users').doc(uid).set({
-  //     'image': url_new,
-  //     'name': name,
-  //     'home_address': home,
-  //     'business_address': business,
-  //     'shopping_address': shop,
-  //     'home_latlng': GeoPoint(homeLatLng!.latitude, homeLatLng.longitude),
-  //     'business_latlng':
-  //     GeoPoint(businessLatLng!.latitude, businessLatLng.longitude),
-  //     'shopping_latlng':
-  //     GeoPoint(shoppingLatLng!.latitude, shoppingLatLng.longitude),
-  //   },SetOptions(merge: true)).then((value) {
-  //     isProfileUploading(false);
-  //
-  //     Get.to(() => HomeScreen());
-  //   });
-  // }
-
-  // var myUser = UserModel().obs;
-
-  // getUserInfo() {
-  //   String uid = FirebaseAuth.instance.currentUser!.uid;
-  //   FirebaseFirestore.instance
-  //       .collection('users')
-  //       .doc(uid)
-  //       .snapshots()
-  //       .listen((event) {
-  //     myUser.value = UserModel.fromJson(event.data()!);
-  //   });
-  // }
-
-  // Future<Prediction?> showGoogleAutoComplete(BuildContext context) async {
-  //   Prediction? p = await PlacesAutocomplete.show(
-  //     offset: 0,
-  //     radius: 1000,
-  //     strictbounds: false,
-  //     region: "pk",
-  //     language: "en",
-  //     context: context,
-  //     mode: Mode.overlay,
-  //     apiKey: AppConstants.kGoogleApiKey,
-  //     components: [new Component(Component.country, "pk")],
-  //     types: [],
-  //     hint: "Search City",
-  //   );
-  //
-  //   return p;
-  // }
-
-  // Future<LatLng> buildLatLngFromAddress(String place) async {
-  //   List<geoCoding.Location> locations =
-  //   await geoCoding.locationFromAddress(place);
-  //   return LatLng(locations.first.latitude, locations.first.longitude);
-  // }
-  //
-  //
-  //
-  // storeDriverProfile(
-  //     File? selectedImage,
-  //     String name,
-  //     String email, {
-  //       String url = '',
-  //
-  //     }) async {
-  //   String url_new = url;
-  //   if (selectedImage != null) {
-  //     url_new = await uploadImage(selectedImage);
-  //   }
-  //   String uid = FirebaseAuth.instance.currentUser!.uid;
-  //   FirebaseFirestore.instance.collection('users').doc(uid).set({
-  //     'image': url_new,
-  //     'name': name,
-  //     'email': email,
-  //     'isDriver': true
-  //   },SetOptions(merge: true)).then((value) {
-  //     isProfileUploading(false);
-  //
-  //     Get.off(()=> CarRegistrationTemplate());
-  //
-  //
-  //
-  //   });
-  // }
-  //
-  //
-  //
-  // Future<bool> uploadCarEntry(Map<String,dynamic> carData)async{
-  //   bool isUploaded = false;
-  //   String uid = FirebaseAuth.instance.currentUser!.uid;
-  //
-  //   await FirebaseFirestore.instance.collection('users').doc(uid).set(carData,SetOptions(merge: true));
-  //
-  //   isUploaded = true;
-  //
-  //   return isUploaded;
-  // }
 
 }
