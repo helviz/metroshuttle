a1e51df13806dd122df4a28a4f2107cf788bc91d
helviz <twomoelvisrodney@gmail.com>
2024-07-03 21:09:46 +0300

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:metroshuttle/views/maps.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:metroshuttle/views/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'controller/auth_controller.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: Home(),
-  ));
+
+
+void main() async {
+  Get.put(AuthController(), permanent: true);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const Maps();
+
+    final textTheme = Theme.of(context).textTheme;
+
+    return GetMaterialApp(
+      theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(textTheme)
+      ),
+      home: const LoginScreen(),
+
+    );
   }
 }
