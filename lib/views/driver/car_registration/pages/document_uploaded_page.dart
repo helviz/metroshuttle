98721b56c812a584802e30a7eec8ced778f852c5
naomiapio11 <naomiapio11@gmail.com>
2024-07-09 23:44:12 +0600

    changes made to document_uploaded_page

--- lib/views/driver/car_registration/pages/document_uploaded_page.dart
+++ lib/views/driver/car_registration/pages/document_uploaded_page.dart
@@ -26,3 +26,39 @@ class _DocumentUploadedPageState extends State<DocumentUploadedPage> {
 
     SizedBox(height: 30,),
 
+
+        Container(
+          width: Get.width,
+          height: Get.height*0.1,
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Color(0xffE3E3E3).withOpacity(0.4),
+              border: Border.all(color: Color(0xff2FB654).withOpacity(0.26),width: 1)
+          ),
+          child: Row(
+            children: [
+              Icon(Icons.cloud_upload,size: 40,color: Color(0xff7D7D7D),),
+
+              const SizedBox(width: 10,),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start ,
+                children: [
+
+                  Text('Vehicle Registration',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,color: Colors.black),),
+                  Text('waiting For Approval',style: TextStyle(fontSize: 12,color: Color(0xff62B62F)),),
+
+
+                ],
+              ),
+            ],
+          ),
+        ),
+
+
+      ],
+    );
+  }
+}
