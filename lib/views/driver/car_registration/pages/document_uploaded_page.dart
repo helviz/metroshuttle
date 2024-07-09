908cf22eb68565aeac9bb9e7f245b535611a5846
naomiapio11 <naomiapio11@gmail.com>
2024-07-09 23:41:41 +0600

    Updated document_uploaded_page

---
+++ lib/views/driver/car_registration/pages/document_uploaded_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DocumentUploadedPage extends StatefulWidget {
+  const DocumentUploadedPage({Key? key}) : super(key: key);
+
+
+
+  @override
+  State<DocumentUploadedPage> createState() => _DocumentUploadedPageState();
+}
+
+class _DocumentUploadedPageState extends State<DocumentUploadedPage> {
