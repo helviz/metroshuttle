2cdfffb01323ad134a4bcc4fe3bdacc50d67fd41
elainek256 <elaynekkiconco441@gmail.com>
2024-07-10 08:38:19 +0300

    Update on the submit button, before editing

--- lib/views/profile_settings.dart
+++ lib/views/profile_settings.dart
@@ -218,6 +218,58 @@ class _ProfileSettingScreenState extends State<ProfileSettingScreen> {
     );
   }
 
+  TextFieldWidget(
+      String title, IconData iconData, TextEditingController controller,Function validator,{Function? onTap,bool readOnly = false}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: GoogleFonts.poppins(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: Color(0xffA7A7A7)),
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Container(
+          width: Get.width,
+          // height: 50,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    spreadRadius: 1,
+                    blurRadius: 1)
+              ],
+              borderRadius: BorderRadius.circular(8)),
+          child: TextFormField(
+            readOnly: readOnly,
+            onTap: ()=> onTap!(),
+            validator: (input)=> validator(input),
+            controller: controller,
+            style: GoogleFonts.poppins(
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+                color: Color(0xffA7A7A7)),
+            decoration: InputDecoration(
+              prefixIcon: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Icon(
+                  iconData,
+                  color: AppColors.greenColor,
+                ),
+              ),
+              border: InputBorder.none,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
 
 
 
