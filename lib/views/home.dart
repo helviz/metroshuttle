629148cae095951ac35d79ddc905e77dc3cde4dd
Itianit Jude <doperudxhark@gmail.com>
2024-07-10 14:04:01 +0300

    Added buildDrawer widget

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -397,6 +397,136 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  buildDrawer() {
+    return Drawer(
+      child: Column(
+        children: [
+          InkWell(
+            onTap: () {
+              Get.to(() => const MyProfile());
+            },
+            child: SizedBox(
+              height: 150,
+              child: DrawerHeader(
+                  child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    width: 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: authController.myUser.value.image == null
+                            ? const DecorationImage(
+                                image: AssetImage('assets/person.png'),
+                                fit: BoxFit.fill)
+                            : DecorationImage(
+                                image: NetworkImage(
+                                    authController.myUser.value.image!),
+                                fit: BoxFit.fill)),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Good Morning, ',
+                            style: GoogleFonts.poppins(
+                                color: Colors.black.withOpacity(0.28),
+                                fontSize: 14)),
+                        Text(
+                          authController.myUser.value.name == null
+                              ? "Mark"
+                              : authController.myUser.value.name!,
+                          style: GoogleFonts.poppins(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                buildDrawerItem(title: 'Payment History', onPressed: () => Get.to(()=> PaymentScreen())),
+                buildDrawerItem(
+                    title: 'Ride History', onPressed: () {}, isVisible: true),
+                buildDrawerItem(title: 'Invite Friends', onPressed: () {}),
+                buildDrawerItem(title: 'Promo Codes', onPressed: () {}),
+                buildDrawerItem(title: 'Settings', onPressed: () {}),
+                buildDrawerItem(title: 'Support', onPressed: () {}),
+                buildDrawerItem(title: 'Log Out', onPressed: () {
+
+                  FirebaseAuth.instance.signOut();
+
+                }),
+              ],
+            ),
+          ),
+          Spacer(),
+          Divider(),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: Column(
+              children: [
+                buildDrawerItem(
+                    title: 'Do more',
+                    onPressed: () {},
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black.withOpacity(0.15),
+                    height: 20),
+                const SizedBox(
+                  height: 20,
+                ),
+                buildDrawerItem(
+                    title: 'Get food delivery',
+                    onPressed: () {},
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black.withOpacity(0.15),
+                    height: 20),
+                buildDrawerItem(
+                    title: 'Make money driving',
+                    onPressed: () {},
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black.withOpacity(0.15),
+                    height: 20),
+                buildDrawerItem(
+                  title: 'Rate us on store',
+                  onPressed: () {},
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black.withOpacity(0.15),
+                  height: 20,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
+
 
   late Uint8List markIcons;
 
