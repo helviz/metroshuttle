16f69571e0d77376050e274b2a43ac631f0f7969
Itianit Jude <doperudxhark@gmail.com>
2024-07-10 09:57:25 +0300

    Added build current location icon widget

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -292,3 +292,20 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  Widget buildCurrentLocationIcon() {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 30, right: 8),
+        child: CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.green,
+          child: Icon(
+            Icons.my_location,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
