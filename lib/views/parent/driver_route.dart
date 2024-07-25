0feb401f5ea3fc532a8a497f258a1099dd51beef
naomiapio11 <naomiapio11@gmail.com>
2024-07-25 17:24:15 +0600

    created a driver_route.dart file

--- lib/views/parent/driver_route.dart
+++ lib/views/parent/driver_route.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter-toast/fluttertoast.dart';
 
 class ParentDriverRoute extends StatefulWidget {
   @override
